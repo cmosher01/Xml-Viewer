0616fb40f9b2583d8aa2a9399c92723ec904297e
Christopher A. Mosher <cmosher01@gmail.com>
2019-11-18 18:30:23 -0500

    add file ags; add build scripts; refactor; (work in progress)

--- src/main/java/nu/mine/mosher/xml/XmlViewer.java
+++ src/main/java/nu/mine/mosher/xml/XmlViewer.java
@@ -1,6 +1,6 @@
 package nu.mine.mosher.xml;
 
-import javafx.application.Application;
+import javafx.application.*;
 import javafx.beans.binding.*;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.geometry.*;
@@ -23,39 +23,82 @@ import java.util.function.Function;
 
 
 public class XmlViewer extends Application {
-//    private static final Image[] images = loadImages();
-
     private ReadOnlyObjectWrapper<Optional<Node>> currentNode = new ReadOnlyObjectWrapper<>(Optional.empty());
 
     public static void main(final String... args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage stage) throws IOException, ParserConfigurationException, SAXException {
-        setUserAgentStylesheet(STYLESHEET_MODENA);
-        final List<String> p = getParameters().getRaw();
-
-        final URL xml;
-        if (!p.isEmpty()) {
-            xml = URI.create(p.get(0)).toURL();
-        } else {
-            xml = Paths.get("./contconc.ged.xml").toUri().toURL();
+    private static URL asUrl(final String pathOrUrl) throws IOException {
+        Throwable urlExcept ;
+        try {
+            return new URI(pathOrUrl).toURL();
+        } catch (final Throwable e) {
+            urlExcept = e;
         }
 
-        final URL urlSchema1 = Paths.get("../../github_cmosher01/Gedcom-To-Xml/lib/relaxng/gedcom.xsd").toUri().toURL();
+        Throwable pathExcept ;
+        try {
+            return Paths.get(pathOrUrl).toUri().toURL();
+        } catch (final Throwable e) {
+            pathExcept = e;
+        }
 
-        final TreeView<Node> treeView = createTree(xml, Collections.singleton(urlSchema1));
-        SplitPane.setResizableWithParent(treeView, Boolean.FALSE);
-        final ScrollPane propsView = new ScrollPane(createProps());
-        propsView.setFitToWidth(true);
-        final SplitPane splitPane = new SplitPane(treeView, propsView);
+        final IOException except = new IOException("Invalid path or URL: "+pathOrUrl);
+        except.addSuppressed(pathExcept);
+        except.addSuppressed(urlExcept);
+        throw except;
+    }
 
-        final Scene scene = new Scene(splitPane, 1024, 1024);
-        scene.getStylesheets().add("stylesheet.css");
-        stage.setScene(scene);
+    @Override
+    public void start(final Stage stage) throws IOException, ParserConfigurationException, SAXException {
+        Platform.runLater(() -> {
+            final List<String> p = getParameters().getUnnamed();
 
-        stage.show();
+            if (p.contains("--help")) {
+                System.out.println("usage: xml-viewer [input.xml [schema.xsd [...]]]");
+                System.out.flush();
+                Platform.exit();
+                return;
+            }
+
+
+            try {
+                final Set<URL> schemata = new HashSet<>();
+                URL xml = null;
+                if (0 < p.size()) {
+                    xml = asUrl(p.get(0));
+                    for (int i = 1; i < p.size(); ++i) {
+                        schemata.add(asUrl(p.get(i)));
+                    }
+                }
+
+                if (Objects.isNull(xml)) {
+                    // TODO: no args provided, so ask user for xml and (optionally) schema files
+                    System.out.println("usage: xml-viewer [input.xml [schema.xsd [...]]]");
+                    System.out.flush();
+                    Platform.exit();
+                    return;
+                }
+
+
+                final TreeView<Node> treeView = createTree(xml, schemata);
+
+                setUserAgentStylesheet(STYLESHEET_MODENA);
+                SplitPane.setResizableWithParent(treeView, Boolean.FALSE);
+                final ScrollPane propsView = new ScrollPane(createProps());
+                propsView.setFitToWidth(true);
+                final SplitPane splitPane = new SplitPane(treeView, propsView);
+
+                final Scene scene = new Scene(splitPane, 1024, 1024);
+                scene.getStylesheets().add("stylesheet.css");
+                stage.setScene(scene);
+
+                stage.show();
+            } catch (final Throwable e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private static class NodeProp {
@@ -72,8 +115,8 @@ public class XmlViewer extends Application {
     private List<NodeProp> prpdefs = new ArrayList<>();
     {
         prpdefs.add(new NodeProp("node type", Node.class, node -> nameFromTypeId(node.getNodeType())));
-        prpdefs.add(new NodeProp("namespace URI", Node.class, Node::getNamespaceURI));
-        prpdefs.add(new NodeProp("namespace prefix", Node.class, Node::getPrefix));
+        prpdefs.add(new NodeProp("ns URI", Node.class, Node::getNamespaceURI));
+        prpdefs.add(new NodeProp("ns prefix", Node.class, Node::getPrefix));
         prpdefs.add(new NodeProp("local name", Node.class, Node::getLocalName));
         prpdefs.add(new NodeProp("tag name", Element.class, node -> ((Element)node).getTagName()));
         prpdefs.add(new NodeProp("node name", Node.class, Node::getNodeName));
@@ -100,7 +143,6 @@ public class XmlViewer extends Application {
         int row = 0;
         for (final NodeProp prop : prpdefs) {
             final javafx.scene.text.Text labelLabel = new javafx.scene.text.Text(prop.label + ":");
-//            final Label labelLabel = new Label(prop.getKey() + ":");
             props.add(labelLabel, LABEL_COL, row);
             GridPane.setValignment(labelLabel, VPos.TOP);
             GridPane.setHalignment(labelLabel, HPos.LEFT);
@@ -169,66 +211,6 @@ public class XmlViewer extends Application {
         return tree;
     }
 
-//    private TreeTableView<Node> createTreeTableView(final URL xml, final Set<URL> schemata) throws IOException, SAXException, ParserConfigurationException {
-//        final TreeTableView<Node> treeTableView = new TreeTableView<>(createTreeItems(xml, schemata));
-//        treeTableView.setShowRoot(true);
-//        treeTableView.setColumnResizePolicy(TreeTableView.CONSTRAINED_RESIZE_POLICY);
-//        treeTableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-//        treeTableView.getSelectionModel().setCellSelectionEnabled(false);
-//
-//
-//        final TreeTableColumn<Node, TreeItem<Node>> nameColumn = new TreeTableColumn<>("Name");
-//        nameColumn.setCellValueFactory(c -> new ReadOnlyObjectWrapper<>(c.getValue()));
-//        nameColumn.setCellFactory(c -> new TreeTableCell<Node, TreeItem<Node>>() {
-//            @Override
-//            protected void updateItem(TreeItem<Node> item, boolean empty) {
-//                super.updateItem(item, empty);
-//                if (empty || Objects.isNull(item) || Objects.isNull(item.getValue()) || item.getValue().getNodeName().isEmpty()) {
-//                    setText(null);
-//                    setGraphic(null);
-//                    setStyle("");
-//                    return;
-//                }
-//                setText(filter(item.getValue().getNodeName()));
-//                setStyle("-fx-border-color: lightgray lightgray red lightgray; -fx-background-color: " + backColor(item.getValue().getNodeType()) + ";");
-//                setGraphic(new ImageView(images[typeIndex(item.getValue().getNodeType())]));
-//            }
-//        });
-//        nameColumn.setSortable(false);
-//        treeTableView.getColumns().add(nameColumn);
-//
-//
-//        final TreeTableColumn<Node, String> valueColumn = new TreeTableColumn<>("Value");
-//        valueColumn.setCellValueFactory(c -> {
-//            final TreeItem<Node> item = c.getValue();
-//            final Node node = item.getValue();
-//            final String s = Objects.isNull(node) ? null : node.getNodeValue();
-//
-//            if (node.getNodeType() == Node.TEXT_NODE) {
-//                final Text tnode = (Text) node;
-//                final String typ = tnode.isElementContentWhitespace() ? "WHITE" : "TEXT";
-//                return new ReadOnlyObjectWrapper<>(filter(typ + ": " + s));
-//            }
-//
-//            return new ReadOnlyObjectWrapper<>(filter(s));
-//        });
-//        valueColumn.setSortable(false);
-//        treeTableView.getColumns().add(valueColumn);
-//
-//
-//        treeTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-//            if (Objects.isNull(newValue) || Objects.isNull(newValue.getValue())) {
-//                this.currentNode.set(Optional.empty());
-//            } else {
-//                this.currentNode.set(Optional.ofNullable(newValue.getValue()));
-//            }
-//        });
-//
-//        treeTableView.getSelectionModel().selectFirst();
-//
-//        return treeTableView;
-//    }
-
     private static class NodeInfo {
         final Class<?> typeClass;
         final String typeName;
@@ -267,30 +249,34 @@ public class XmlViewer extends Application {
     private static final String W3C_XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
 
     private TreeItem<Node> createTreeItems(final URL xml, final Set<URL> schemata) throws ParserConfigurationException, IOException, SAXException {
+        final boolean schema = (0 < schemata.size());
+
         final DocumentBuilderFactory factoryDocBuild = DocumentBuilderFactory.newInstance();
-        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/schema", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/validate-annotations", true);
         factoryDocBuild.setFeature("http://apache.org/xml/features/honour-all-schemaLocations", true);
-//        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/cta-full-xpath-checking", true);
-//        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/assert-comments-and-pi-checking", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/warn-on-duplicate-attdef", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/warn-on-undeclared-elemdef", true);
         factoryDocBuild.setFeature("http://apache.org/xml/features/warn-on-duplicate-entitydef", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/scanner/notify-char-refs", true);
-        factoryDocBuild.setFeature("http://apache.org/xml/features/scanner/notify-builtin-refs", true);
         factoryDocBuild.setFeature("http://apache.org/xml/features/standard-uri-conformant", true);
         factoryDocBuild.setFeature("http://apache.org/xml/features/xinclude", true);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/validate-annotations", true);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/schema-full-checking", true);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/schema", schema);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/warn-on-duplicate-attdef", true);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/validation/warn-on-undeclared-elemdef", true);
+        factoryDocBuild.setFeature("http://apache.org/xml/features/scanner/notify-char-refs", true);
+//        factoryDocBuild.setFeature("http://apache.org/xml/features/scanner/notify-builtin-refs", true); // crashes
         factoryDocBuild.setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
         factoryDocBuild.setNamespaceAware(true);
-        factoryDocBuild.setValidating(true);
+        factoryDocBuild.setValidating(schema);
+        factoryDocBuild.setExpandEntityReferences(false);
+        factoryDocBuild.setCoalescing(false);
+        factoryDocBuild.setIgnoringElementContentWhitespace(false);
+        factoryDocBuild.setIgnoringComments(false);
         factoryDocBuild.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
         factoryDocBuild.setAttribute(JAXP_SCHEMA_SOURCE, schemata.stream().map(URL::toExternalForm).toArray());
 
         final DocumentBuilder builder = factoryDocBuild.newDocumentBuilder();
         builder.setErrorHandler(new MyErrorHandler(new PrintWriter(new OutputStreamWriter(new FileOutputStream(FileDescriptor.err), StandardCharsets.UTF_8), true)));
 
-        final Document doc = builder.parse(xml.openStream());
+        final Document doc = builder.parse(xml.toExternalForm());
 
         final TreeItem<Node> root = new TreeItem<>(doc);
         addChildrenItem(root);
@@ -307,6 +293,8 @@ public class XmlViewer extends Application {
                 final Node attr = attrs.item(i);
                 final TreeItem<Node> treeItem = new TreeItem<>(attr);
                 root.getChildren().add(treeItem);
+
+                addChildrenItem(treeItem);
             }
         }
 
@@ -320,28 +308,6 @@ public class XmlViewer extends Application {
         }
     }
 
-//    private int typeIndex(int t) {
-//        switch (t) {
-//            case Node.ELEMENT_NODE:
-//                return 1;
-//            case Node.ATTRIBUTE_NODE:
-//                return 2;
-//            case Node.TEXT_NODE:
-//                return 3;
-//            case Node.CDATA_SECTION_NODE:
-//                return 4;
-//            case Node.PROCESSING_INSTRUCTION_NODE:
-//                return 5;
-//            case Node.COMMENT_NODE:
-//                return 6;
-//            case Node.DOCUMENT_NODE:
-//                return 7;
-//            case Node.DOCUMENT_TYPE_NODE:
-//                return 8;
-//        }
-//        return 0;
-//    }
-
     private static String backColor(short nodeType) {
         switch (nodeType) {
             case Node.ELEMENT_NODE:
@@ -353,15 +319,6 @@ public class XmlViewer extends Application {
         }
         return "lightgray";
     }
-
-//    private static Image[] loadImages() {
-//        Image[] images = new Image[9]; // MUST MATCH NUMBER OF ICONS IN PNG FILE
-//        Image image = new Image(XmlViewer.class.getResourceAsStream("image2.png"));
-//        for (int i = 0; i < images.length; i++) {
-//            images[i] = new WritableImage(image.getPixelReader(), i * 16, 0, 16, 16);
-//        }
-//        return images;
-//    }
 
     private static String filter(String s) {
         if (Objects.isNull(s)) {
@@ -376,7 +333,7 @@ public class XmlViewer extends Application {
 
 
     private static class MyErrorHandler implements ErrorHandler {
-        private PrintWriter out;
+        private final PrintWriter out;
 
         MyErrorHandler(PrintWriter out) {
             this.out = out;
